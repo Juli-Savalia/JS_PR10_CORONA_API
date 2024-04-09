79020665e6190580781733dc7a8118dcfc3b2c95
Juli <3382julisavalia@gmail.com>
2024-04-09 10:53:23 +0530

    apii

--- api.js
+++ api.js
@@ -1,14 +1,32 @@
 const URL = "https://dummyjson.com/posts";
-let para = document.querySelector("p");
+let data = document.querySelector("posts");
 let btn = document.querySelector( 'button' );
 
 let corona = async () => {
     console.log("loading data..");
     let response = await fetch(URL);
-    console.log(response);
+    // console.log(response);
     let data = await response.json();
-    console.log(data);
-    para.innerText = data.posts[4].title;
+    // console.log(data);
+    // para.innerText = data.posts[4].title;
 
+    let allposts =  data.posts;
+    console.log(allposts);
+    let tbl="";
+    allposts.map((post)=>{
+        tbl += `
+                <div class="d-flex">
+                    <div class="card w-100" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${post.title}</h5>
+                            <p class="card-text">${post.body}</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+              `
+    })
+
+    document.getElementById("posts").innerHTML = tbl;
 }
-btn.addEventListener('click', corona);
+btn.addEventListener('click', corona)
